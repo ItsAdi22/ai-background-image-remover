1288b260edc6629de8766437f8889c395ad92666
Aditya-Laptop <adityamondkar111@gmail.com>
2023-10-29 12:58:07 +0530

    using flask now

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from rembg import remove
-import easygui
+from flask import Flask, render_template, request, redirect, url_for
+import os
 from PIL import Image
+from rembg import remove  # Make sure to install the rembg library
 
-inputPath = easygui.fileopenbox(title='Select image file')
-outputPath = easygui.filesavebox(title='Save file to...')
+app = Flask(__name__)
 
-input_image = Image.open(inputPath)
-output_image = remove(input_image)
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-output_image.save(outputPath)
+@app.route('/output', methods=['POST'])
+def output():
+    img = request.files['img']
+    
+    if img:
+        # Save the uploaded image
+        img_path = os.path.join('static/input', img.filename)
+        img.save(img_path)
+
+        # Process the uploaded image using rembg
+        input_image = Image.open(img_path)  # Assuming you have PIL installed
+        output_image = remove(input_image)
+        output_path = os.path.join('static/output', img.filename)
+        output_image.save(output_path)
+        
+        return render_template('output.html', input_image=img_path, output_image=output_path)
+
+    return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
