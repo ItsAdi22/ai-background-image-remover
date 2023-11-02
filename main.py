e99f8f81280f48805d4c6c21f37aa02685d40b35
Aditya-Laptop <adityamondkar111@gmail.com>
2023-11-02 20:00:18 +0530

    test

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from rembg import remove
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     try:
@@ -26,25 +27,38 @@ def home():
 
     finally:
         return render_template('home.html')
+    
+
 
 @app.route('/output', methods=['POST'])
 def output():
     img = request.files['img']
+
     
     if img:
         # Save the uploaded image
         img_path = os.path.join('static/input', img.filename)
         img.save(img_path)
 
+
+
+
         # Process the uploaded image using rembg
-        input_image = Image.open(img_path)  # Assuming you have PIL installed
-        output_image = remove(input_image)
-        output_path = os.path.join('static/output', img.filename)
-        output_image.save(output_path)
+        try:
+            input_image = Image.open(img_path)  # Assuming you have PIL installed
+            output_image = remove(input_image)
+            output_path = os.path.join('static/output', img.filename)
+            output_image.save(output_path)
+        except Exception as e:
+            print(f"Error Occurred: {e}")
+            return render_template('home.html',error="Error: Please select a different image")
         
+            
         return render_template('output.html', input_image=img_path, output_image=output_path)
 
     return redirect(url_for('home'))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
